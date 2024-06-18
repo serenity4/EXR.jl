cb220e5efef0312a53fe7f56e860d4c0844e25ca
Documenter.jl <documenter@juliadocs.github.io>
2024-06-18 17:44:25 +0000

    build based on 51b97eb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EXR","category":"page"},{"location":"#EXR","page":"Home","title":"EXR","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EXR.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EXR]","category":"page"}]
+}
